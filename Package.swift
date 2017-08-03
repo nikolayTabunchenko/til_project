721cc4ed90fd48bd076af6b44ff3d664858a2f0a
Nikolay Tabunchenko <nikolay@invoodoo.com>
2017-08-03 22:57:02 +0300

    Initial build.

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-08-03 14:17:27 +0000
+import PackageDescription
+let package = Package(
+	name: "TIL",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
+	]
+)
